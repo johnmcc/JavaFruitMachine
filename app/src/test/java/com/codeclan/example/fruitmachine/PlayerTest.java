ccc95943659c5d63f269248c4a1845356f5ccef3
John McCollum <johnvmccollum@gmail.com>
2017-06-30 10:41:46 +0100

    Add initial Player tests

--- app/src/test/java/com/codeclan/example/fruitmachine/PlayerTest.java
+++ app/src/test/java/com/codeclan/example/fruitmachine/PlayerTest.java
@@ -1,8 +1,30 @@
 package com.codeclan.example.fruitmachine;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 30/06/2017.
  */
 
 public class PlayerTest {
+    Player player;
+
+    @Before
+    public void setUp() throws Exception {
+        Player player = new Player(10);
+    }
+
+    @Test
+    public void testGetMoney() throws Exception {
+        assertEquals(10, player.getMoney());
+    }
+
+    @Test
+    public void testAddMoney() throws Exception {
+        player.addMoney(5);
+        assertEquals(15, player.getMoney());
+    }
 }
